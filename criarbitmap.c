1a75ca46d7cc482255a4e0bddff9b14858d8f70f
vitorstraggiotti <vitorstraggiotti@hotmail.com>
2020-04-20 14:57:18 -0300

    adicionando arquivos

---
+++ criarbitmap.c
@@ -0,0 +1,97 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Codigo fonte da biblioteca para criar imagens do tipo bitmap (.bmp)         *
+ *                                                                             *
+ * Autor: Vitor Henrique Andrade Helfensteller Satraggiotti Silva              *
+ * Iniciado em: 18/04/2020                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//undigned char = 1
+//unsigned short int  = 2
+//unsignde int = 4
+#include<stdlib.h>
+#include<stdio.h>
+#include"criarbitmap.h"
+
+//===============================================================================
+//função que inicializa o arquivo de imagem
+header_t inicializa_bmp(unsigned short int Precisao, unsigned int Largura, unsigned int Altura, unsigned int NumCoresTabela){
+  header_t cabecalho;
+  unsigned int BytesTabela;       //tamanho em bytes da tabela de cores
+
+  //verificação de tamanho das variaveis
+  if(sizeof(unsigned char) != 1){
+    printf("Erro ao criar arquivo de cabecalho! Sem compatibilidade nas variaveis do sistema.\n");
+    exit(1);
+  }elseif(sizeof(unsigned short int) != 2){
+    printf("Erro ao criar arquivo de cabecalho! Sem compatibilidade nas variaveis do sistema.\n");
+    exit(1);
+  }elseif(sizeof(unsigned int) != 4){
+    printf("Erro ao criar arquivo de cabecalho! Sem compatibilidade nas variaveis do sistema.\n");
+    exit(1);
+  }
+
+  //Preenchendo valores fixos do cabeçalho
+  //----------------------------------------------------------------------------
+  cabecalho.IdentidadeChar_1 = 'B';
+  cabecalho.IdentidadeChar_2 = 'M';
+  cabecalho.Reservado_1 = 0;
+  cabecalho.Reservado_2 = 0;
+  cabecalho.TamanhoHeader2 = 40;
+  cabecalho.Largura = Largura;     ////impor limites as dimenções da imagem
+  cabecalho.Altura = Altura;
+  cabecalho.Planos = 1;
+  cabecalho.Compressao = 0;
+  cabecalho.ResolucaoX = RESOLUCAO_X;
+  cabecalho.ResolucaoY = RESOLUCAO_Y;
+  cabecalho.CoresImportantes = 0;
+
+  //verificação de entrada de variavel "Precisão"
+  if((Precisao==1)||(Precisao==4)||(Precisao==8)||(Precisao==16)||(Precisao==24)){
+    cabecalho.Precisao = Precisao;
+  }else{
+    printf("Erro! Valor de bits por pixel incorreto.\n");
+    exit(1);
+  }
+  //verificação de entrada de variavel "NumCoresTabela"
+  if(((Precisao==16)||(Precisao==24)) && (NumCoresTabela!=0)){
+    printf("Erro! 16 e 24 bits por pixel nao aceita tabela de cores.\n");
+    exit(1);
+  }elseif((Precisao==1) && (NumCoresTabela>2)){
+    printf("Erro! 1 bit por pixel não aceita mais que 2 cores na tabela.\n");
+    exit(1);
+  }elseif((Precisao==4) && (NumCoresTabela>16)){
+    printf("Erro! 4 bits por pixel não aceita mais que 16 cores na tabela.\n");
+    exit(1);
+  }elseif((Precisao==8) && (NumCoresTabela>256)){
+    printf("Erro! 8 bits por pixel não aceita mais que 256 cores na tabela.\n");
+    exit(1);
+  }else{
+    cabecalho.NumCoresTabela = NumCoresTabela;
+  }
+
+  //Preenchendo valores calculaveis do cabeçalho
+  //----------------------------------------------------------------------------
+  //Encontrando tamanho da tabela de cores
+  if(NumCoresTabela==0){
+    if(Precisao==1) BytesTabela = 8;
+    if(Precisao==4) BytesTabela = 64;
+    if(Precisao==8) BytesTabela = 1024;
+    if(Precisao==16) BytesTabela = 0;
+    if(Precisao==24) BytesTabela = 0;
+  }else{
+    BytesTabela = 4 * NumCoresTabela;
+  }
+  cabecalho.OffsetMatrizPixel = 54 + BytesTabela;
+
+  //Encontrando tamanho da matriz de pixel
+  unsigned int BitsLinha;         //tamanho em bits de uma linha da imagem
+  BitsLinha = Largura * Precisao; //sem padding
+  if((BitsLinha%32)!=0){
+    BitsLinha = BitsLinha + 32 - (BitsLinha%32); //adicionando bits para alinhamento de memoria
+  }
+  cabecalho.TamanhoMatrizPixel = (BitsLinha/8) * Altura;
+
+  //Encontrando tamanho total do arquivo
+  cabecalho.TamanhoArquivo = 54 + BytesTabela + cabecalho.TamanhoMatrizPixel;
+
+  return cabecalho;
+}
---
+++ criarbitmap.h
@@ -0,0 +1,58 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Arquivo de cabeçalho da biblioteca para criar imagens do tipo bitmap (.bmp) *
+ *                                                                             *
+ * Autor: Vitor Henrique Andrade Helfensteller Satraggiotti Silva              *
+ * Iniciado em: 18/04/2020                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/***************************** MACROS *****************************************/
+//resolução em pixel por metro (39,3701 * DPI)
+#define RESOLUCAO_X   2835
+#define RESOLUCAO_Y   2835
+//numero de cores utilizadas na tabela
+#define TABELA_COMPLETA  0
+#define SEM_TABELA       0
+
+/*************************** ESTRUTURAS ***************************************/
+//configura temporariamente o alinhamento de memoria para 1 byte
+#pragma pack(push, 1)
+//estrutura de cabeçalho do arquivo (14 bytes)
+struct header{
+  unsigned char IdentidadeChar_1;           //Código para identificação de arquivo BMP
+  unsigned char IdentidadeChar_2;           //Código para identificação de arquivo BMP
+  unsigned int TamanhoArquivo;              //Tmanho total do arquivo (headers + tabela + matriz pixel) em byte
+  unsigned short int Reservado_1;           //Espaço reservado para utilização por outros programas
+  unsigned short int Reservado_2;           //Espaço reservado para utilização por outros programas
+  unsigned int OffsetMatrizPixel;           //Desvio em bytes do inicio da matriz de pixel (header_1 + header_2 + tabela)
+  //------------------------------------------------------------------------------------
+  //estrutura de caeçalho da imagem versão BITMAPINFOHEADER (40 bytes)
+  unsigned int TamanhoHeader2;            //Tamanho do header_2 em bytes
+  unsigned int Largura;                   //Largura da imagem em pixel (esquerda ==> direita)
+  unsigned int Altura;                    //Altura da imagem em pixel (positivo de baixo ==> cima)
+  unsigned short int Planos;              //Numero de planos de cor sendo utilizados
+  unsigned short int Precisao;            //Quantos Bits por pixel (1, 4, 8, 16, 24, 32)
+  unsigned int Compressao;                //Tipo de compressão utilizada (0 ==> sem compressão)
+  unsigned int TamanhoMatrizPixel;        //Tamanho em bytes da matriz de pixel incluindo padding
+  unsigned int ResolucaoX;                //Resolução de impressão em pixel por metro (39,3701 * DPI) em X
+  unsigned int ResolucaoY;                //Resolução de impressão em pixel por metro (39,3701 * DPI) em Y
+  unsigned int NumCoresTabela;            //Numero de cores presente na tabela de cores (usado quando bpp<=8 e =0 se usar todas)
+  unsigned int CoresImportantes;          //Numero de cores importantes (normalmente zero)
+};
+
+//estrutura de pixel de 24 bits para tabela de cores
+struct pixel_tabela_cores{
+  unsigned char Vermelho;
+  unsigned char Verde;
+  unsigned char Azul;
+  unsigned char Reservado;
+};
+#pragma pack(pop)
+
+/************************ REDEFINIÇÕES DE RÓTULOS *****************************/
+//redefinição de rotulos das estruturas
+typedef struct header                header_t;
+typedef struct pixel_tabela_cores    pixeltabela_t;
+
+/******************************* FUNÇÕES ***************************************/
+//inicializando arquivo de imagem (retorna estrutura de cabeçalho)
+header_t inicializa_bmp(unsigned short int Precisao, unsigned int Largura, unsigned int Altura, unsigned int NumCoresTabela);
